83fce32b3922bcd5a60d4926a0ed677e0d48b2dd
AsyaAndreeva <asiara032@gmail.com>
2024-09-19 12:19:55 +0300

    Added navigation

--- apple-website-demo/src/App.jsx
+++ apple-website-demo/src/App.jsx
@@ -1,33 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from "./pages";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 const App = () => {
   return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
 };
 
 export default App;
---
+++ apple-website-demo/src/components/Highlights.jsx
@@ -0,0 +1,7 @@
+const Highlights = () => {
+  return (
+    <div>Highlights</div>
+  )
+}
+
+export default Highlights
