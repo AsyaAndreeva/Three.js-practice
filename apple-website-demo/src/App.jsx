362a439109b596547612a8aa4fbda1ca90543c44
AsyaAndreeva <asiara032@gmail.com>
2024-09-30 10:58:48 +0300

    Added Sentry

--- apple-website-demo/src/App.jsx
+++ apple-website-demo/src/App.jsx
@@ -3,7 +3,10 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 
+import * as Sentry from '@sentry/react';
+
 const App = () => {
+
   return (
     <main className="bg-black">
       <Navbar />
@@ -14,4 +17,4 @@ const App = () => {
   )
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- apple-website-demo/src/main.jsx
+++ apple-website-demo/src/main.jsx
@@ -1,7 +1,28 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://afd00daae8abd8d7e7f79d73e1a1d2c9@o4508040654946304.ingest.de.sentry.io/4508040657240144",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({useEffect: React.useEffect,}),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
