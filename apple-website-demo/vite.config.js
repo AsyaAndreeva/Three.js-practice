bcab62979d6fdddefe49a2800b169d5c8abffa53
AsyaAndreeva <asiara032@gmail.com>
2024-10-01 15:29:43 +0300

    Updated

--- apple-website-demo/vite.config.js
+++ apple-website-demo/vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-2fn",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
 
   build: {
     sourcemap: true,
